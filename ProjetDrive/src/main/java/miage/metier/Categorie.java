e66d6ceec5e494cb54dc2df25d27cbf06d1b537c
hopelessdream222 <hopelessdream222@outlook.com>
2021-03-22 21:27:05 +0800

    getter/setter

--- ProjetDrive/src/main/java/miage/metier/Categorie.java
+++ ProjetDrive/src/main/java/miage/metier/Categorie.java
@@ -39,5 +39,75 @@ public class Categorie {
     // relation <Appartenir>
     @OneToMany(mappedBy = "categorieP", cascade= CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Produit> produits = new HashSet<>(0);
+
+    public Categorie() {
+    }
+    
+    public Categorie(String libelleCat, Rayon rayonCat) {
+        this.libelleCat = libelleCat;
+        this.rayonCat = rayonCat;
+    }
+
+    public int getIdCat() {
+        return idCat;
+    }
+
+    public void setIdCat(int idCat) {
+        this.idCat = idCat;
+    }
+
+    public String getLibelleCat() {
+        return libelleCat;
+    }
+
+    public void setLibelleCat(String libelleCat) {
+        this.libelleCat = libelleCat;
+    }
+
+    public Rayon getRayonCat() {
+        return rayonCat;
+    }
+
+    public void setRayonCat(Rayon rayonCat) {
+        this.rayonCat = rayonCat;
+    }
+
+    public Set<Produit> getProduits() {
+        return produits;
+    }
+
+    public void setProduits(Set<Produit> produits) {
+        this.produits = produits;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" + "idCat=" + idCat + ", libelleCat=" + libelleCat + ", rayonCat=" + rayonCat + ", produits=" + produits + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + this.idCat;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categorie other = (Categorie) obj;
+        if (this.idCat != other.idCat) {
+            return false;
+        }
+        return true;
+    }
     
 }
--- ProjetDrive/src/main/java/miage/metier/LigneCommandeId.java
+++ ProjetDrive/src/main/java/miage/metier/LigneCommandeId.java
@@ -19,4 +19,54 @@ public class LigneCommandeId implements Serializable{
     private int idP;
     @Column(name="idCmd")  
     private int idCmd;
+
+    public LigneCommandeId() {
+    }
+
+    public int getIdP() {
+        return idP;
+    }
+
+    public void setIdP(int idP) {
+        this.idP = idP;
+    }
+
+    public int getIdCmd() {
+        return idCmd;
+    }
+
+    public void setIdCmd(int idCmd) {
+        this.idCmd = idCmd;
+    }
+
+    @Override
+    public String toString() {
+        return "LigneCommandeId{" + "idP=" + idP + ", idCmd=" + idCmd + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.idP;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LigneCommandeId other = (LigneCommandeId) obj;
+        if (this.idP != other.idP) {
+            return false;
+        }
+        return true;
+    }
+    
 }
