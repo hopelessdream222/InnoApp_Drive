57cee8f39d9af9c48eb517a0d22c504db6f34803
CHENMenglin96 <menglinchen96@gmail.com>
2021-03-22 16:29:18 +0100

    image

--- ProjetDrive/src/main/java/miage/dao/TestHibernate.java
+++ ProjetDrive/src/main/java/miage/dao/TestHibernate.java
@@ -1,5 +1,11 @@
 package miage.dao;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import static java.lang.reflect.Array.set;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Iterator;
@@ -31,23 +37,50 @@ public class TestHibernate
 
 	/*----- Création et enregistrement d'employés -----*/
 	public static void chercherCinqProduits ()
-		{
-		/*----- Ouverture de la session -----*/
-		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
-			{
-			/*----- Ouverture d'une transaction -----*/
-			Transaction t = session.beginTransaction();
-			// ...
-                        t.commit(); // Commit et flush automatique de la session.
-			}
-		}
+        {
+        /*----- Ouverture de la session -----*/
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+                {
+                /*----- Ouverture d'une transaction -----*/
+                Transaction t = session.beginTransaction();
+                // ...
+                t.commit(); // Commit et flush automatique de la session.
+                }
+        }
 
+        public static void loadPhotos () throws FileNotFoundException, IOException, SQLException
+        {
+        /*----- Ouverture de la session -----*/
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+                {
+                /*----- Ouverture d'une transaction -----*/
+                Transaction t = session.beginTransaction();
+                    List<Produit> liste = session.createQuery("from Produit").list();
+                    for (Produit p : liste)
+                    {
+                        InputStream inputStream = p.getPhotoP().getBinaryStream();
+                        FileOutputStream fos = new FileOutputStream("src\\main\\webapp\\image\\"+p.getIdP()+".jpg");
+                        byte[] b = new byte[1024];
+                        int len = -1;
+                        while ((len = inputStream.read(b)) != -1) {
+                            fos.write(b, 0, len);
+                        }
+                        fos.close();
+                        inputStream.close();
+                    }                   
+                // ...
+                t.commit(); // Commit et flush automatique de la session.
+                }
+        }
+        
+        
 	/**
 	 * Programme de test.
 	 */
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException, FileNotFoundException, SQLException 
 		{
                    TestHibernate.chercherCinqProduits();
+                    //TestHibernate.loadPhotos();
 		/*----- Exit -----*/
 		System.exit(0);
 		}
