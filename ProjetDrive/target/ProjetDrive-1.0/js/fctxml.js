4ee86719c796afff559379adce29847a2e251680
meng7777 <66383612+meng7777@users.noreply.github.com>
2021-03-23 09:42:54 +0100

    image

--- ProjetDrive/target/ProjetDrive-1.0/js/fctxml.js
+++ ProjetDrive/target/ProjetDrive-1.0/js/fctxml.js
@@ -88,9 +88,10 @@ function testEncodeUrl ()
 	xhr.send(param);
 	}
 
-
+window.onload=afficheDetail();
 function afficheDetail ()
 	{
+            alert("123");
 	// Objet XMLHttpRequest.
 	var xhr = new XMLHttpRequest();
 
@@ -98,17 +99,17 @@ function afficheDetail ()
 	xhr.open("GET","ServletDetailProd");
 
 	// On précise ce que l'on va faire quand on aura reçu la réponse du serveur.
-	xhr.onload = function()
-		{
-		// Si la requête http s'est bien passée.
-		if (xhr.status === 200)
-                    {
-                        
+	xhr.onload = function(){
+            // Si la requête http s'est bien passée.
+            if (xhr.status === 200){  
+                for(var i=2; i<=5;i++){
                     // Elément html que l'on va mettre à jour.
-                    var elt = document.getElementById("image2_prodId");
-                    elt.innerHTML = xhr.responseText;
-                    }
-		};
+                    var elt = document.getElementById("image"+i);
+                    elt.innerHTML = "<img src='"+xhr.responseXML.getElementsByTagName("src")[0].firstChild.nodeValue+"'>";
+                    document.getElementById("image"+i+"_prodId").innerHTML=xhr.responseXML.getElementsByTagName("idProd")[0].firstChild.nodeValue;
+                }
+            }
+        };
 	
 	// Envoie de la requête.
 	xhr.send();
